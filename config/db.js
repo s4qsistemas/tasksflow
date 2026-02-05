4c05a5de29e7dcb39d00697133253a8d9f9fc288
Diego Ignacio Alvial Arrepol <sigm4q@gmail.com>
2026-02-05 18:19:59 -0300

    Actualización método de conexión a la base de datos MySQL en Xammp

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'tasksflow',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME,
   charset: 'utf8mb4',
   waitForConnections: true,
   connectionLimit: 10,
